64c6336dae6c7f76a68d9fda887ae298a1eeb066
Douglas W Schultz <vxpx17@gmail.com>
2017-12-31 04:22:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+
+function theBeatlesPlay(musiArr,instArr){
+  var result=[];
+  for(var i=0;i<musiArr.length;i++){
+    result.push(`${musiArr[i]} plays ${instArr[i]}`)}
+    return result;}
+
+function johnLennonFacts(factsArr){
+  var i=0;
+while(factsArr[i]<factsArr.length){
+  factsArr[i]+="!!!";
+  i++;}
+  return factsArr;}
+  
+function iLoveTheBeatles(n){
+  var result=[];
+  do {result.push("I love the Beatles!");n++;}while(n<15);
+  return result;
+}
+
+facts[0]+="!!!";
+console.log(facts[0]);
+console.log(facts[0]+"!!!");
